52b71f221cc3fe15716179f52f24561cd2790a17
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2019-01-02 13:00:21 +0100

    [FIX] Remove content-encoding from fowarded headers

--- odoo_invader/views.py
+++ odoo_invader/views.py
@@ -80,7 +80,7 @@ class OdooApi(APIView):
             # http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html#sec13.5.1
             'connection', 'keep-alive', 'proxy-authenticate',
             'proxy-authorization', 'te', 'trailers', 'transfer-encoding',
-            'upgrade',
+            'upgrade', 'content-encoding',
         ])
         for key, value in odoo_response.headers.items():
             if key.lower() in excluded_headers:
